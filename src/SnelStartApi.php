b057fbb97aa420db4d4edd0c49210d471075facb
sandertotalwebb <sander@totalwebboost.nl>
2018-02-08 17:47:18 +0100

    fix

--- src/SnelStartApi.php
+++ src/SnelStartApi.php
@@ -4,7 +4,7 @@ namespace s1351\SnelStartApi;
 
 use s1351\SnelStartApi\Client\Auth;
 use s1351\SnelStartApi\Actions\Artikelen as ArtikelenAction;
-use s1351\SnelStartApi\Actions\RelatiesOrders as RelatiesAction;
+use s1351\SnelStartApi\Actions\Relaties as RelatiesAction;
 use s1351\SnelStartApi\Actions\VerkoopOrders as VerkoopOrdersAction;
 
 class SnelStartApi
